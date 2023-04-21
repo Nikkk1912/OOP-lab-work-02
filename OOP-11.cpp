22bd0eae7bcc275f51fb7e3def81028351422718
nikkk1912 <moskalevskiy1@gmail.com>
2023-04-21 19:57:49 +0300

    task 11 complete

--- OOP-11.cpp
+++ OOP-11.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -13,94 +14,106 @@ const int cseconds = 1;
 
 class Time {
 private:
-    int hour = 0;
-    int minute = 0;
-    int second = 0;
-    int totalSeconds = 0;
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+  int totalSeconds = 0;
 
 public:
-    Time() {
-        
-    }
+  Time() {}
 
-    Time(int h, int m, int s) : hour(h), minute(m), second(s), totalSeconds((h * 60 + m) * 60 + s) {}
+  Time(int h, int m, int s)
+      : hour(h), minute(m), second(s), totalSeconds((h * 60 + m) * 60 + s) {}
 
-    int getHour() const { return hour; }
-    int getMinute() const { return minute; }
-    int getSecond() const { return second; }
+  int getHour() const { return hour; }
+  int getMinute() const { return minute; }
+  int getSecond() const { return second; }
 
-    static Time readFromConsole() {
-        int hour, minute, second;
-        cin >> hour >> minute >> second;
-        return Time(hour, minute, second);        
-    }
+  static Time readFromConsole() {
+    int hour, minute, second;
+    cin >> hour >> minute >> second;
+    return Time(hour, minute, second);
+  }
 
-    Time operator-(const Time& n)  {
-        int resultSeconds = totalSeconds - n.totalSeconds;
-        
-        if(resultSeconds < 0)
+  Time operator-(const Time &n) {
+    
+    int seconds = second - n.second;
+        if(seconds < 0)
         {
-            resultSeconds =+ totalSeconds + (cseconds_in_day - n.totalSeconds);
+            minute--;
+            second += 60;
+
+            seconds = second - n.second;
         }
 
-        int hours = (resultSeconds % cseconds_in_day) / cseconds_in_hour;
-        int minutes = ((resultSeconds % cseconds_in_day) % cseconds_in_hour) / cseconds_in_minute;
-        int seconds = (((resultSeconds % cseconds_in_day) % cseconds_in_hour) % cseconds_in_minute) / cseconds;
+    int minutes = minute - n.minute;
+        if(minutes < 0)
+        {
+            hour--;
+            minute += 60;
 
-        return Time(hours, minutes, seconds);
-    }
+            minutes = minute - n.minute;
+        }
 
-    bool operator>(const Time& n) const {
-        return totalSeconds > n.totalSeconds;
-    }
+    int hours = hour - n.hour;
+        if(hours < 0)
+        {
+            
+            hour += 24;
 
-    bool operator<(const Time& n) const {
-        return totalSeconds < n.totalSeconds;
-    }
+            hours = hour - n.hour;
+        }
 
-    bool operator!=(const Time& n) const {
-        return totalSeconds != n.totalSeconds;
-    }
+    return Time(hours, minutes, seconds);
+  }
 
-    int getTotalSeconds() const {
-        return totalSeconds;
-    }
-    
+  bool operator>(const Time &n) const { return totalSeconds > n.totalSeconds; }
+
+  bool operator<(const Time &n) const { return totalSeconds < n.totalSeconds; }
+
+  bool operator!=(const Time &n) const {
+    return totalSeconds != n.totalSeconds;
+  }
+
+  int getTotalSeconds() const { return totalSeconds; }
 };
 
-ostream& operator<<(ostream& stream, const Time& t)
-{
-    stream << setfill('0') << setw(2) << t.getHour() << ":" << setw(2) << t.getMinute() << ":" << setw(2) << t.getSecond();
-    return stream;
+ostream &operator<<(ostream &stream, const Time &t) {
+  stream << setfill('0') << setw(2) << t.getHour() << ":" << setw(2)
+         << t.getMinute() << ":" << setw(2) << t.getSecond();
+  return stream;
 };
-
 
 int main() {
-    int n;
-    
-    cin >> n;
-    vector<Time> vec;
-    Time min_dif(INT_MAX, INT_MAX, INT_MAX);
-    
+  int n;
 
-    for(auto i = 0; i <n; i++)
-    {
-        Time temp = Time::readFromConsole();
-        vec.push_back(temp);
+  cin >> n;
+  vector<Time> vec;
+  Time min_dif(INT_MAX, INT_MAX, INT_MAX);
+
+  for (auto i = 0; i < n; i++) {
+    Time temp = Time::readFromConsole();
+    vec.push_back(temp);
+  }
+
+  sort(vec.begin(), vec.end(), greater <>());
+
+  for (int i = 0; i < vec.size() - 1; i++) {
+
+    for (int i2 = i + 1; i2 < vec.size(); i2++) {
+      Time time = vec[i2] - vec[i];
+      Time time1 = vec[i] - vec[i2];
+
+      if (time < min_dif && time.getTotalSeconds() >= 0) {
+        min_dif = time;
+      }
+      if (time1 < min_dif && time.getTotalSeconds() >= 0) {
+        min_dif = time1;
+      }
     }
-    
-    for (int i = 0; i < vec.size() - 1; i++) {
+  }
 
-        for (int i2 = i + 1; i2 < vec.size(); i2++) {
-            Time time = vec[i2] - vec[i];
-            
-            if (time < min_dif && time.getTotalSeconds() >= 0) {
-                min_dif = time;
-            }
-        }
-    }
-    
-    cout << min_dif;
+  cout << min_dif;
 
-    return 0;
+  return 0;
 }
